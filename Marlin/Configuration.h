613689eb039713ed809d8e9cf4330b6739b30082
jtoplak <jjamestoplak@gmail.com>
2021-12-05 13:32:28 -0800

    switch back to ABL and hardcode new z offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1181,7 +1181,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 22.8, -41, -3.48 }
+#define NOZZLE_TO_PROBE_OFFSET { 22.8, -41, -2.70 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1540,8 +1540,8 @@
  */
 //#define AUTO_BED_LEVELING_3POINT
 //#define AUTO_BED_LEVELING_LINEAR
-//#define AUTO_BED_LEVELING_BILINEAR
-#define AUTO_BED_LEVELING_UBL
+#define AUTO_BED_LEVELING_BILINEAR
+//#define AUTO_BED_LEVELING_UBL
 //#define MESH_BED_LEVELING
 
 /**
